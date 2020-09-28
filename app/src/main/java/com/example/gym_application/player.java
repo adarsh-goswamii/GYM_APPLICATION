7426d19dfe7acfcc8e28e7e90dd05b2742c13f83
Adarsh Goswami <adarsh010120@gmail.com>
2020-09-28 09:19:35 +0530

    player

---
+++ app/src/main/java/com/example/gym_application/player.java
@@ -0,0 +1,14 @@
+package com.example.gym_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
