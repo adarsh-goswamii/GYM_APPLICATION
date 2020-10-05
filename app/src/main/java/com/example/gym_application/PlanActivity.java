fe8c7fdea11999356ff3d5bfce6fa862192f1da8
Adarsh Goswami <adarsh010120@gmail.com>
2020-10-05 08:30:00 +0530

    commit 2

--- app/src/main/java/com/example/gym_application/PlanActivity.java
+++ app/src/main/java/com/example/gym_application/PlanActivity.java
@@ -11,9 +11,11 @@ import android.widget.TextView;
 
 public class PlanActivity extends AppCompatActivity {
 
-    private RelativeLayout mondayRelative;
-    private RecyclerView mondayRecycler;
-    private ImageView downMonday, upMonday;
+    private RelativeLayout mondayRelative, sundayRelative, tuesdayRelative, wednesdayRelative, thursdayRelative, fridayRelative, saturdayRelative;
+    private RecyclerView mondayRecycler, tuesdayRecycler, wednesdayRecycler, thursdayRecycler, fridayRecycler, saturdayRecycler, sundayRecycler ;
+    private ImageView downMonday, upMonday, downTuesday, upTuesday, downWednesday, upWednesday, downThursday, upThursday, downFriday, upFriday, downSaturday, upSaturday;
+    private ImageView downSunday, upSunday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,115 @@ public class PlanActivity extends AppCompatActivity {
                 mondayRecycler.setVisibility(View.GONE);
             }
         });
+
+        downTuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downTuesday.setVisibility(View.GONE);
+                upTuesday.setVisibility(View.VISIBLE);
+                tuesdayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
+        upTuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upTuesday.setVisibility(View.GONE);
+                downTuesday.setVisibility(View.VISIBLE);
+                tuesdayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        upWednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upWednesday.setVisibility(View.GONE);
+                downWednesday.setVisibility(View.VISIBLE);
+                wednesdayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        downWednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downWednesday.setVisibility(View.GONE);
+                upWednesday.setVisibility(View.VISIBLE);
+                wednesdayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
+        downThursday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downThursday.setVisibility(View.GONE);
+                upThursday.setVisibility(View.VISIBLE);
+                thursdayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
+        upThursday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upThursday.setVisibility(View.GONE);
+                downThursday.setVisibility(View.VISIBLE);
+                thursdayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        upFriday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upFriday.setVisibility(View.GONE);
+                downFriday.setVisibility(View.VISIBLE);
+                fridayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        downFriday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downFriday.setVisibility(View.GONE);
+                upFriday.setVisibility(View.VISIBLE);
+                fridayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
+        downSaturday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downSaturday.setVisibility(View.GONE);
+                upSaturday.setVisibility(View.VISIBLE);
+                saturdayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
+        upSaturday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upSaturday.setVisibility(View.GONE);
+                downSaturday.setVisibility(View.VISIBLE);
+                saturdayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        upSunday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upSunday.setVisibility(View.GONE);
+                downSunday.setVisibility(View.VISIBLE);
+                sundayRecycler.setVisibility(View.GONE);
+            }
+        });
+
+        downSunday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downSunday.setVisibility(View.GONE);
+                upSunday.setVisibility(View.VISIBLE);
+                sundayRecycler.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 
     private void initView()
@@ -46,5 +157,35 @@ public class PlanActivity extends AppCompatActivity {
         mondayRelative= findViewById(R.id.mondayRelLayout);
         upMonday= findViewById(R.id.upArrow);
         downMonday= findViewById(R.id.downArrow);
+
+        tuesdayRecycler= findViewById(R.id.mondayRecyler);
+        tuesdayRelative= findViewById(R.id.mondayRelLayout);
+        upTuesday= findViewById(R.id.upArrow);
+        downTuesday= findViewById(R.id.downArrow);
+
+        wednesdayRecycler= findViewById(R.id.mondayRecyler);
+        wednesdayRelative= findViewById(R.id.mondayRelLayout);
+        upWednesday= findViewById(R.id.upArrow);
+        downWednesday= findViewById(R.id.downArrow);
+
+        thursdayRecycler= findViewById(R.id.mondayRecyler);
+        thursdayRelative= findViewById(R.id.mondayRelLayout);
+        upThursday= findViewById(R.id.upArrow);
+        downThursday= findViewById(R.id.downArrow);
+
+        fridayRecycler= findViewById(R.id.mondayRecyler);
+        fridayRelative= findViewById(R.id.mondayRelLayout);
+        upFriday= findViewById(R.id.upArrow);
+        downFriday= findViewById(R.id.downArrow);
+
+        saturdayRecycler= findViewById(R.id.mondayRecyler);
+        saturdayRelative= findViewById(R.id.mondayRelLayout);
+        upSaturday= findViewById(R.id.upArrow);
+        downSaturday= findViewById(R.id.downArrow);
+
+        sundayRecycler= findViewById(R.id.mondayRecyler);
+        sundayRelative= findViewById(R.id.mondayRelLayout);
+        upSunday= findViewById(R.id.upArrow);
+        downSunday= findViewById(R.id.downArrow);
     }
 }
